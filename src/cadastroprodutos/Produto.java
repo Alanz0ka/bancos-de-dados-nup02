0a0e00bf97a6acf598318c1b69b2755e1b0629eb
José Alan <104922063+Alanz0ka@users.noreply.github.com>
2026-05-31 14:33:58 -0300

    Entidade Produto

---
+++ src/cadastroprodutos/Produto.java
@@ -0,0 +1,78 @@
+package cadastroprodutos;
+
+/**
+ * Entidade que representa um produto da tabela "produto" do banco de dados.
+ * Cada atributo corresponde a uma coluna da tabela.
+ */
+public class Produto {
+
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+    private String categoria;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, double preco, int quantidade, String categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+    }
+
+    public Produto(int id, String nome, double preco, int quantidade, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("#%-3d | %-22s | R$ %9.2f | qtd: %4d | %s",
+                id, nome, preco, quantidade, categoria);
+    }
+}
